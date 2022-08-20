2b4d30d253ef85fcb3c2f4977afb9a044a06e028
linya <cdthanye32@163.com>
2022-08-21 07:49:28 +0800

    feat(.): 重新构建webpack学习目录

---
+++ 01demo-Webpack基础配置逻辑/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "./dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/i,
+        include: {
+          and: [path.join(__dirname, './src/')]
+        },
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "less-loader",
+          }
+        ]
+      }
+    ]
+  }
+};
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # sturdy-octo-dollop
 webapck demos
+
+# 目录
+
+[业务中常用的webpack基础配置](./02-webpack-baseconfig-business/README.md)
